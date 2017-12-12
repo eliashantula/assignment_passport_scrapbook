0441aae5d64f9b4dfb516984cf8b84c8c70223e9
Elias Hantula <ehantula@indiana.edu>
2017-12-12 11:15:28 -0500

    setup

---
+++ app.js
@@ -0,0 +1,173 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+const flash = require("express-flash");
+const FacebookStrategy = require("passport-facebook").Strategy;
+var index = require('./routes/index');
+var users = require('./routes/users');
+const User = require("./models/User");
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/passport-demo")
+var app = express();
+const LocalStrategy = require("passport-local").Strategy;
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+const passport = require("passport");
+app.use(passport.initialize());
+app.use(passport.session());
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+//Routes
+
+
+
+app.get("/", (req, res) => {
+  if (req.user) {
+    res.render("home", { user: req.user });
+  } else {
+    res.redirect("/login");
+  }
+});
+
+// 2
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+
+
+// 3
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true
+  })
+);
+
+// 4
+app.post("/register", (req, res, next) => {
+  const { username, password } = req.body;
+  const user = new User({ username, password });
+  user.save((err, user) => {
+    req.login(user, function(err) {
+      if (err) {
+        return next(err);
+      }
+      return res.redirect("/"); });
+  });
+});
+
+// 5
+app.get("/logout", function(req, res) {
+  req.logout();
+  res.redirect("/");
+});
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+app.get("/auth/facebook", passport.authenticate("facebook"));
+
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/",
+    failureRedirect: "/login"
+  })
+);
+
+//Passbook Strategies 
+
+//**Local 
+passport.use(
+  new LocalStrategy(function(username, password, done) {
+    User.findOne({ username }, function(err, user) {
+      console.log(user);
+      if (err) return done(err);
+      if (!user || !user.validPassword(password)) {
+        return done(null, false, { message: "Invalid username/password" });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+//**Facebook
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      callbackURL: "http://localhost:4000/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      const facebookId = profile.id;
+      const displayName = profile.displayName;
+
+      console.log(profile);
+      User.findOne({ facebookId }, function(err, user) {
+        if (err) return done(err);
+
+        if (!user) {
+          // Create a new account if one doesn't exist
+          user = new User({ facebookId, displayName });
+          user.save((err, user) => {
+            if (err) return done(err);
+            done(null, user);
+          });
+        } else {
+          // Otherwise, return the extant user.
+          done(null, user);
+        }
+      });
+    }
+  )
+);
+
+
+
+
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
